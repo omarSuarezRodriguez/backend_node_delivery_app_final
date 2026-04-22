033528887938630b6862658c0a6b25821c16bc34
omarSuarezRod <omar.suarez.rod@gmail.com>
2026-04-22 11:48:48 -0500

    Vid 23

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Es el backend para desarrollar la API que nos permitirá todo lo que tiene que v
 
 # Anotaciones
 
--en server.js está la IP, que en este caso es la misma del pc para pruebas, si no, con ipconfig en cmd la ipv4 es
+- En server.js está la IP, que en este caso es la misma del pc para pruebas, si no, con ipconfig en cmd la ipv4 es
 
 - 'node server.js' con ese comando arranca el server
+
+- Para detener la aplicación en la terminal, Ctrl + c
--- server.js
+++ server.js
@@ -1,13 +1,59 @@
+// Importamos packages
 const express = require('express');
 const http = require('http');
+const logger = require('morgan');
+const cors = require('cors');
 
+
+// Ejecutamos métodos
 const app = express();
 const server = http.createServer(app);
 
+
+// Puerto
 const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json()); // Para parsear respuestas que recibamos en formato json
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
+
+app.disable('x-powered-by');
+
 app.set('port', port);
 
+
+// Método para arrancar servidor
 // Ipconfig, ip local para probar
 server.listen(3000, '192.168.20.69' || 'localhost', function() {
     console.log('Aplicación de NodeJS ', + port + ' Iniciada...');
 });
+
+
+// Nuestra primera ruta, ruta raiz
+app.get('/', (req, res) => {
+    res.send('Ruta raiz del backend');
+});
+
+// Ruta test
+app.get('/test', (req, res) => {
+    res.send('Ruta test');
+});
+
+
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
+
+
+
+
+// Errores
+// 200 - Es una respuesta Exitosa
+// 400 - La Url no existe
+// 500 - Error interno del servidor (Puede ser que haya que revisar código para solucionar)
